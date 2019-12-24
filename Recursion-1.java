579a5c25027aceb32ba53a1657d4bb2506a14971
Dean Shin <42852604+ShinkaiSetsuna@users.noreply.github.com>
2019-12-23 21:35:44 -0800

    Update Recursion-1.java

--- Recursion-1.java
+++ Recursion-1.java
@@ -117,3 +117,86 @@ public String pairStar(String str) {
     return str.substring(0, 1) + '*' + pairStar(str.substring(1));
   return str.substring(0, 1) + pairStar(str.substring(1));
 }
+
+public String endX(String str) {
+  if(str.length() == 0) return "";
+  if(str.charAt(0) == 'x') return endX(str.substring(1)) + "x";
+  return str.substring(0, 1) + endX(str.substring(1));
+}
+
+public int countPairs(String str) {
+  if(str.length() <= 2) return 0;
+  if(str.charAt(0) == str.charAt(2)) return 1 + countPairs(str.substring(1));
+  return countPairs(str.substring(1));
+}
+
+public int countAbc(String str) {
+  if(str.length() <= 2) return 0;
+  if(str.substring(0,3).equals("abc") || str.substring(0,3).equals("aba")){
+    return 1 + countAbc(str.substring(1));
+  }
+  return countAbc(str.substring(1));
+}
+
+public int count11(String str) {
+  if(str.length() < 2) return 0;
+  if(str.substring(0,2).equals("11")) return 1 + count11(str.substring(2));
+  return count11(str.substring(1));
+}
+
+public String stringClean(String str) {
+  if(str.length() <= 1) return str;
+  if(str.charAt(0) == str.charAt(1)) return stringClean(str.substring(1));
+  return str.charAt(0) + stringClean(str.substring(1));
+}
+
+public int countHi2(String str) {
+  if(str.length() < 2) return 0;
+  if(str.length() > 2 && str.substring(0, 3).equals("xhi")) 
+    return countHi2(str.substring(3));
+  if(str.substring(0,2).equals("hi")) 
+    return 1 + countHi2(str.substring(2));
+  return countHi2(str.substring(1));
+}
+
+public String parenBit(String str) {
+  if (str.charAt(0) != '(') {
+    return parenBit(str.substring(1));
+  }
+  if (str.charAt(str.length()-1) != ')') {
+    return parenBit(str.substring(0, str.length()-1));
+  }
+  return str;
+}
+
+public boolean nestParen(String str) {
+  if(str.length() == 0) return true;
+  if(str.charAt(0) == '(' && str.charAt(str.length()-1) == ')'){
+    return nestParen(str.substring(1,str.length()-1));
+  }
+  return false;
+}
+
+public int strCount(String str, String sub) {
+  if(str.length() < sub.length()) return 0;
+  if(str.substring(0,sub.length()).equals(sub))
+    return 1 + strCount(str.substring(sub.length()), sub);
+  return strCount(str.substring(1), sub);
+}
+
+public boolean strCopies(String str, String sub, int n) {
+  if(n == 0) return true;
+  if(str.length() < sub.length()) return false;
+  if(str.substring(0,sub.length()).equals(sub))
+    return strCopies(str.substring(1), sub, n - 1);
+  return strCopies(str.substring(1), sub, n);
+}
+
+public int strDist(String str, String sub) {
+  if(str.length() < sub.length()) return 0;
+  if(!str.substring(0,sub.length()).equals(sub))
+    return strDist(str.substring(1), sub);
+  if(!str.substring(str.length() - sub.length()).equals(sub))
+    return strDist(str.substring(0,str.length()-1), sub);
+  return str.length();
+}
